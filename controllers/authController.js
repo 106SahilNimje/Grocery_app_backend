f3794cee7fe5ce263a5f3750efcc84194f843f58
106Sahil <co.2022.srnimje@bitwardha.ac.in>
2026-02-14 12:21:05 +0530

    log's checking backend 5

--- controllers/authController.js
+++ controllers/authController.js
@@ -7,11 +7,12 @@ const User = require("../models/User"); // Import User model
 // NOTE: Use environment variables for real credentials
 // Nodemailer Transporter
 // NOTE: Use environment variables for real credentials
+// Nodemailer Transporter
+// NOTE: Use environment variables for real credentials
 const transporter = nodemailer.createTransport({
-    service: "gmail",
     host: "smtp.gmail.com",
-    port: 587,
-    secure: false, // true for 465, false for other ports
+    port: 465,
+    secure: true, // true for 465, false for other ports
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
