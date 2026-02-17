3c6a3a747811fa8e63b9f69c3c636f0015826f3b
106Sahil <co.2022.srnimje@bitwardha.ac.in>
2026-02-17 12:27:58 +0530

    fix the error of image

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(helmet());
+app.use(helmet({ crossOriginResourcePolicy: false }));
 app.use(cors()); // Allow all origins for now to simplify development
 app.use(express.json());
 
